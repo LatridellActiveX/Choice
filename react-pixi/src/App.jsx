3c1ebda7806fc3c4653a889e6f96213506bf6348
Austin Imbastari <fvckimba@Austins-MBP.lan>
2022-09-27 19:43:10 -0700

    initial commit

---
+++ react-pixi/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// styles
+import GlobalStyles from "./Global.styles";
+
+function App() {
+    return (
+        <>
+            <GlobalStyles />
+            <h1>Pixi</h1>
+        </>
+    );
+}
+
+export default App;
